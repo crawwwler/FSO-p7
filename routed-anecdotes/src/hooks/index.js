9eaa08f85c0428f5bdd8ebdefe8a8e57275a8b79
crawwwler <shahinkopite@gmail.com>
2023-10-15 16:53:02 +0330

    add some comments to custom hook

--- routed-anecdotes/src/hooks/index.js
+++ routed-anecdotes/src/hooks/index.js
@@ -3,10 +3,12 @@ import { useState } from "react";
 export const useField = (type) => {
     const [value, setValue] = useState('')
 
+    // CONTROLLING THE INPUT VALUE
     const onChange = (event) => {
         setValue(event.target.value)
     }
 
+    // RESETING THE FIELD
     const clearFunc = () => {
         setValue('')
     }
